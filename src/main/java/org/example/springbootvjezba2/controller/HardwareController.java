f926a5eb3951e582cba0fc1b55298d71d0933bd2
Dinko <dinko.sulicek@predavaci.algebra.hr>
2025-06-09 19:42:48 +0200

    Vjezba 2 . dodan kontroler

---
+++ src/main/java/org/example/springbootvjezba2/controller/HardwareController.java
@@ -0,0 +1,31 @@
+package org.example.springbootvjezba2.controller;
+import org.example.springbootvjezba2.dto.HardwareDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("hardware")
+public class HardwareController {
+
+    private final HardwareService hardwareService;
+
+    public HardwareController(HardwareService hardwareService) {
+        this.hardwareService = hardwareService;
+    }
+
+    @GetMapping
+    public List<HardwareDTO> getAll() {
+        return hardwareService.findAll();
+    }
+
+    @GetMapping(params = "code")
+    public HardwareDTO getByCode(@RequestParam final String code) {
+        return hardwareService.findByCode(code);
+    }
+//    link?code=223232
+
+}
